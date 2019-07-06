faa98ddafe48ac732ce6417cbe6f1562c8131059
ERossDGit <50912378+ERossDGit@users.noreply.github.com>
2019-07-06 18:16:04 -0400

    Fixed filePath misspelling and added response items

--- server.js
+++ server.js
@@ -14,21 +14,25 @@ http
       filePath = "index.html";
     }
 
-    fs.readFile(filepath, function(err, data) {
+    fs.readFile(filePath, function(err, data) {
       if (err) {
         throw err;
       }
-    });
-    fs.appendFile(
-      "log.txt",
-      "URL: " + addr + "\nTimestamp: " + new Date() + "\n\n",
-      function(err) {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log("Added to log.");
+      fs.appendFile(
+        "log.txt",
+        "URL: " + addr + "\nTimestamp: " + new Date() + "\n\n",
+        function(err) {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log("Added to log.");
+          }
         }
-      }
-    );
+      );
+
+      response.writeHead(200, { "Content-Type": "text/html" });
+      response.write(data);
+      response.end();
+    });
   })
   .listen(8080);
