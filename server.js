c41f0820de1856cb0e2abc6d748bfbc9e1196318
ERossDGit <50912378+ERossDGit@users.noreply.github.com>
2019-07-05 21:55:02 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const url = require("url"),
+  http = require("http"),
+  fs = require("fs");
+
+http
+  .createServer((request, response) => {
+    var addr = request.url,
+      q = url.parse(addr, true),
+      filePath = "";
+
+    if (q.pathname.includes("documentation")) {
+      filePath = __dirname + "/documentation.html";
+    } else {
+      filePath = "index.html";
+    }
+
+    fs.readFile(filepath, function(err, data) {
+      if (err) {
+        throw err;
+      }
+    });
+    fs.appendFile(
+      "log.txt",
+      "URL: " + addr + "\nTimestamp: " + new Date() + "\n\n",
+      function(err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Added to log.");
+        }
+      }
+    );
+  })
+  .listen(8080);
